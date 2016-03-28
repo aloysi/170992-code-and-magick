67c2d8dc102becf64b3b52baf6595cc83ebfeb70
aloysi <aloysius@mail.ru>
2016-03-28 15:59:46 +0300

    Переназвала переменную
    + немного добавила пробелов  и переносов.

--- build/js/check.js
+++ build/js/check.js
@@ -1,19 +1,25 @@
 "use strict";
 
 function getMessage(a, b) {
-if(typeof a=="number") {return("Я прыгнул на " + a*100 + " сантиметров");
-} if(typeof a=="boolean" && a==true) {return("Я попал в " + b);
-//тут, видимо, еще должно быть условие, что typeof b=="number"? или если а не массив, то и b не будет массивом в нашем случае?
-} if (typeof a=="boolean" && a==false) {return("Я никуда не попал");
- } if (Array.isArray(a) && Array.isArray(b)) {
- var length=0;
+  if (typeof a=="number") {
+return ("Я прыгнул на " + a*100 + " сантиметров");
+} 
+    if (typeof a=="boolean" && a===true) {
+return("Я попал в " + b);
+} 
+    if (typeof a=="boolean" && a===false) {
+    return("Я никуда не попал");
+}
+    if (Array.isArray(a) && Array.isArray(b)) {
+ var distance=0;
  for (var i = 0; i < a.length; i++) {
-   length += a[i]*b[i];
- } return ("Я прошёл " + length + " метров");
-     } if (Array.isArray(a)) { 
+   distance += a[i]*b[i];
+ } return ("Я прошёл " + distance + " метров");
+}
+    if (Array.isArray(a)) { 
  var sum=0;
  for (var i = 0; i < a.length; i++)  {
-   sum += a[i];
+ sum += a[i];
  } return ("Я прошёл " + sum + " шагов"); 
  }
  }
