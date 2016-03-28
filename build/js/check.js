39805c0c26b600e8c9990f4c2c9251a0d7b375a5
aloysi <aloysius@mail.ru>
2016-03-28 16:44:28 +0300

    Задание 1-2, еще правки
    Добавила еще немного пробелов...

--- build/js/check.js
+++ build/js/check.js
@@ -1,23 +1,24 @@
 "use strict";
 
 function getMessage(a, b) {
-  if (typeof a=="number") {
-return ("Я прыгнул на " + a*100 + " сантиметров");
-} 
-    if (typeof a=="boolean" && a===true) {
-return("Я попал в " + b);
-} 
-    if (typeof a=="boolean" && a===false) {
-    return("Я никуда не попал");
+  
+    if (typeof a === "number") {
+return ("Я прыгнул на " + a * 100 + " сантиметров");
+}
+    if (typeof a === "boolean" && a === true) {
+return ("Я попал в " + b);
+}
+    if (typeof a === "boolean" && a === false) {
+return ("Я никуда не попал");
 }
     if (Array.isArray(a) && Array.isArray(b)) {
- var distance=0;
+ var distance = 0;
  for (var i = 0; i < a.length; i++) {
-   distance += a[i]*b[i];
+   distance += a[i] * b[i];
  } return ("Я прошёл " + distance + " метров");
 }
     if (Array.isArray(a)) { 
- var sum=0;
+ var sum = 0;
  for (var i = 0; i < a.length; i++)  {
  sum += a[i];
  } return ("Я прошёл " + sum + " шагов"); 
