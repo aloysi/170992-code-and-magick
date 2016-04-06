30d53b1eb91e8745934f5cdab80333f46bbab501
aloysi <aloysius@mail.ru>
2016-04-06 14:09:19 +0300

    #9 Валидация - частично :(
    Не получилось пока ничего с радоибаттонами. Не могу найти, как вызвать
    значение выбранной радиокнопки. Пробовала разные варианты вроде
    reviewMark.value.checked  или даже reviewMark[0].checked (хоть для
    одной) - не реагирует.  И еще пришлось залезть в HTML и присвоить
    некоторым элементам Id. Чувствую, что все должно быть элементарно, но
    ...

--- build/js/form.js
+++ build/js/form.js
@@ -15,3 +15,55 @@
     formContainer.classList.add('invisible');
   };
 })();
+
+var names = document.querySelector('#review-name');
+var description = document.getElementById('review-text');
+var reviewMark = document.getElementsByName('review-mark');
+var submitButton = document.getElementById('submit-btn');
+var namesLabel = document.querySelector('#revew-name-label');
+var descriptionLabel = document.querySelector('#revew-description-label');
+var allLabelsBlock = document.querySelector('#review-fields');
+
+submitButton.setAttribute('disabled', 'disabled');
+
+
+var removeDisabled = function() {
+  if (names.value !== '' ) {
+  //if ((names.value !== '' ) && (reviewMark.value.checked > 3)) || ((names.value !== '') && (description.value !== '')) {
+    submitButton.removeAttribute('disabled', 'disabled');
+    namesLabel.style.display = 'none';
+  }
+};
+
+var removeLabel = function() {
+  if ( description.value !== '' ) {
+    descriptionLabel.style.display = 'none';
+  }
+};
+
+var removeAllLabels = function() {
+  if (( names.value !== '' ) && ( description.value !== '' )) {
+    allLabelsBlock.style.display = 'none';
+  }
+};
+
+names.onchange = function() {
+  removeDisabled(names, reviewMark, description);
+  removeAllLabels(names, description);
+};
+
+description.onchange = function() {
+  removeDisabled(names, reviewMark, description);
+  removeLabel(description);
+  removeAllLabels(names, description);
+};
+
+reviewMark.onclick = function() {
+  removeDisabled(names, reviewMark, description);
+};
+
+removeDisabled(names, reviewMark, description);
+
+removeLabel(description);
+
+removeAllLabels(names, description);
