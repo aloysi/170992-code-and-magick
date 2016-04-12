5ca27932833bdc919322080f55321db1cdd287cd
aloysi <aloysius@mail.ru>
2016-04-12 12:44:26 +0300

    Модуль 3 - таск 2, доработки
    Ввела перебор радиобаттонов, заменила oninput вместо onchange, добавила
    ненужную переменную :) Вроде бы в целом заработало, но с тормозами -
    если, например, поставить низкую оценку, ввести имя, а потом заменить
    оценку на высокую, то отправка заработает (без заполнения описания)
    только если вернуться в поле "имя". Id не правила :(

--- build/js/form.js
+++ build/js/form.js
@@ -23,16 +23,24 @@ var submitButton = document.getElementById('submit-btn');
 var namesLabel = document.querySelector('#revew-name-label');
 var descriptionLabel = document.querySelector('#revew-description-label');
 var allLabelsBlock = document.querySelector('#review-fields');
-
+var myMark = 0;
 submitButton.setAttribute('disabled', 'disabled');
 
-
 var removeDisabled = function() {
-  if (names.value !== '' ) {
-  //if ((names.value !== '' ) && (reviewMark.value.checked > 3)) || ((names.value !== '') && (description.value !== '')) {
+  for (var i = 0; i < 5; i++) {
+    if (reviewMark[i].checked) {
+      myMark = i;
+    } }
+
+  if ((names.value !== '' ) && (myMark > 3)) {
     submitButton.removeAttribute('disabled', 'disabled');
     namesLabel.style.display = 'none';
   }
+  if ((names.value !== '') && (description.value !== '')) {
+    submitButton.removeAttribute('disabled', 'disabled');
+    namesLabel.style.display = 'none';
+  }
+
 };
 
 var removeLabel = function() {
@@ -47,20 +55,23 @@ var removeAllLabels = function() {
   }
 };
 
-names.onchange = function() {
+reviewMark.onclick = function() {
+
+  removeDisabled(names, reviewMark, description);
+};
+
+names.oninput = function() {
   removeDisabled(names, reviewMark, description);
   removeAllLabels(names, description);
 };
 
-description.onchange = function() {
+description.oninput = function() {
   removeDisabled(names, reviewMark, description);
   removeLabel(description);
   removeAllLabels(names, description);
 };
 
-reviewMark.onclick = function() {
-  removeDisabled(names, reviewMark, description);
-};
+
 
 removeDisabled(names, reviewMark, description);
 
