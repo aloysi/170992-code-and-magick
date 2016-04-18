83ce10387de45b8e9a0bb3a72565d7b1060d96ae
aloysi <aloysius@mail.ru>
2016-04-18 16:04:06 +0300

    Модуль 4 таск 1
    Не загружаются картинки :(((

--- src/reviews.js
+++ src/reviews.js
@@ -5,7 +5,7 @@ var reviewsContainer = document.querySelector('.reviews-list');
 var templateElement = document.querySelector('template');
 var elementToClone;
 
-reviewsFilter.setAttribute('invisible', 'invisible');
+reviewsFilter.classList.add('invisible');
 
 if ('content' in templateElement) {
   elementToClone = templateElement.content.querySelector('.review');
@@ -15,11 +15,30 @@ if ('content' in templateElement) {
 
 var getReviewElement = function(data, container) {
   var element = elementToClone.cloneNode(true);
-  element.querySelector('.review-author').textContent = data.author;
+  element.querySelector('.review-rating').textContent = data.rating;
+  element.querySelector('.review-text').textContent = data.description;
   container.appendChild(element);
+
+  var userpic = new Image();
+  //var userpic = element.querySelector('.review-author');
+
+  userpic.onload = function() {
+    element.style.background = 'url("../build/' + userpic.src + '")';
+    element.style.width = '124px';
+    element.style.height = '124px';
+  };
+
+  userpic.onerror = function() {
+    element.classList.add('review-load-failure');
+  };
+
+  userpic.src = data.picture;
+
   return element;
 };
 
 reviews.forEach(function(review) {
   getReviewElement(review, reviewsContainer);
 });
+
+reviewsFilter.classList.remove('invisible');
