a6e4e3a7036258ed5ef84cba3458b263098cac9e
aloysi <aloysius@mail.ru>
2016-04-19 15:20:21 +0300

    Модуль 4 - таск 1
    Внесла правки

--- src/reviews.js
+++ src/reviews.js
@@ -5,6 +5,14 @@ var reviewsContainer = document.querySelector('.reviews-list');
 var templateElement = document.querySelector('template');
 var elementToClone;
 
+var ratingStarsClassName = {
+  '1': 'review-rating-one',
+  '2': 'review-rating-two',
+  '3': 'review-rating-three',
+  '4': 'review-rating-four',
+  '5': 'review-rating-five'
+};
+
 reviewsFilter.classList.add('invisible');
 
 if ('content' in templateElement) {
@@ -13,26 +21,35 @@ if ('content' in templateElement) {
   elementToClone = templateElement.querySelector('.review');
 }
 
+/** @constant {number} */
+var IMAGE_LOAD_TIMEOUT = 10000;
+
 var getReviewElement = function(data, container) {
   var element = elementToClone.cloneNode(true);
-  element.querySelector('.review-rating').textContent = data.rating;
+
+  var image = element.querySelector('img');
+
+  element.querySelector('.review-rating').classList.add(ratingStarsClassName[data['rating']]);
   element.querySelector('.review-text').textContent = data.description;
   container.appendChild(element);
 
-  var userpic = new Image();
-  //var userpic = element.querySelector('.review-author');
+  var userpic = new Image(124, 124);
 
   userpic.onload = function() {
-    element.style.background = 'url("../build/' + userpic.src + '")';
-    element.style.width = '124px';
-    element.style.height = '124px';
+    clearTimeout(userpicLoadTimeout);
+    image.setAttribute('src', data.author.picture);
   };
+
+  userpic.src = data.author.picture;
+
+  var userpicLoadTimeout = setTimeout(function() {
+    image.src = '';
+    image.classList.add('review-load-failure');
+  }, IMAGE_LOAD_TIMEOUT);
 
   userpic.onerror = function() {
     element.classList.add('review-load-failure');
   };
-
-  userpic.src = data.picture;
 
   return element;
 };
